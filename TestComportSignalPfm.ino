f4ee8c0344c9f52954a765769d02fe969ecb66bc
mikerobredo <elrobredo@gmail.com>
2019-03-19 13:09:46 +0100

    Create TestComportSignalPfm.ino

---
+++ TestComportSignalPfm.ino
@@ -0,0 +1,40 @@
+
+int input =10;
+unsigned long duration,duration2,duration3;
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("intruzca los numeros");
+  pinMode(10 , input);
+  myservo.attach(9);
+}
+
+void loop() {
+
+  for (pos = 0; pos <= 180; pos += 1) { 
+   
+    myservo.write(pos);             
+  duration = pulseIn(10,HIGH);
+  //duration3 = pulseIn(10,LOW);
+  duration2 =map(duration, 544, 2400, 0, 180);
+  //Serial.println(duration2);
+  //Serial.println(duration3);
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  myservo.write(pos);              
+  duration = pulseIn(10,HIGH);
+  //duration3 = pulseIn(10,LOW);
+  duration2 =map(duration, 544, 2400, 0, 180);
+  //Serial.println(duration2);
+  //Serial.println(duration3);
+  }
+  
+ 
+  
+}
